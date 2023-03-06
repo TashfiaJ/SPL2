2e79c5c28301b62090e905c8e254e7fa2abf0237
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-03-06 12:31:47 +0600

    Create main.py

---
+++ src/Login/main.py
@@ -0,0 +1,9 @@
+app = FastAPI(title='Contact.ly', description='APIs for contact Apis', version='0.1')
+
+@app.on_event("startup")
+async def startup():
+    print("Connecting...")
+
+@app.get("/")
+async def root():
+    return {"message": "Contact Applications!"}
